0d81060900953f1f64f6b67b754d6a104fdff1ee
Wania Faraz <waniafarazk@gmail.com>
2026-04-01 16:42:29 -0400

    Update instructor_home_page_scripts.js

--- public/scripts/instructor_home_page_scripts.js
+++ public/scripts/instructor_home_page_scripts.js
@@ -1,10 +1,29 @@
 console.log('scripts loaded');
-document.addEventListener('DOMContentLoaded', loadHomePage); //do loadHomePage when the document loads
+document.addEventListener('DOMContentLoaded', getSession); //do getSession when the document loads
+
+const PORT = 8080;
+const URL = `http://localhost${PORT}`;
+let userId;
+
+//WAITING FOR INSTRUCTOR SESSION AND SIGN IN IS DEFINED
+async function getSession() {
+    const session = await fetch();
+    userId = session.userId;
+    loadHomePage();
+}
+    
 
 async function loadHomePage() {
-    loadCourses();
+    await loadCourses();
 }
 
 async function loadCourses() {
+    //temmporarily hardcode instructor id
+    const instructorId = 2001;
+    const response = await fetch(`${URL}/api/instructor/get-courses/${instructorId}`);
+    const courseIds = await response.json();
+    const courses = courseIds.forEach( async (value, index, array) => {
+        response = await fetch(`${URL}/api/instructor`)
+    })
     
 }
