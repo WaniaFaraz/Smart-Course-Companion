d1eabf7055814111fe1b72afdf6027530305aa67
Wania Faraz <waniafarazk@gmail.com>
2026-04-05 17:25:23 -0400

    updated

--- public/scripts/instructor_home_page_scripts.js
+++ public/scripts/instructor_home_page_scripts.js
@@ -339,32 +339,4 @@ async function loadAnnouncements() {
 
 
 
-/** TECHNICALLY FOR STUDENTS...MOVE THERE
-//GENERATE DROPDOWN OPTIONS FOR THE COURSE SECTION IN THE MODAL FOR WHEN A COURSE IS ADDED
-const courseCodeInput = document.getElementById("ask-code");
-courseCodeInput.addEventListener('keyup', async function() {
-    console.log("reached event listener");
-    console.log(courseCodeInput.value);
-    if(courseCodeInput.value.length == 8) { //a valid course code should have 8 characters: 'AAAA 111'
-        //fetch the route to get course sections from ids
-        //remove spaces from the course code - to avoid errors
-        const courseCode = courseCodeInput.value.slice(0,4) + courseCodeInput.value.slice(5);
-        const response = await fetch(`/api/instructor/get-sections-from-course-code/${courseCode}`);
-        const sections = await response.json(response);
-        console.log("possible sections:",sections);
-        const element = document.getElementById("course-select-section");
-        generateSectionsDropDown(sections, element);
-    }
-})
 
-async function generateSectionsDropDown(sections, element) {
-    //sections is an array of sections
-    //element is the html element where the dropdown needs to be inserted
-    if(sections.length == 0) { //invalid course, i.e. has no sections
-        element.innerHTML += `<option >Invalid</option>`;
-    }
-    sections.forEach((value, index, array) => {
-        element.innerHTML += `<option value=${value}>${value}</option>`;
-    })
-}
-*/
