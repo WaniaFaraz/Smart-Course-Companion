87da4373c9990d13069649f7ccc29c61a21a2389
Wania Faraz <waniafarazk@gmail.com>
2026-05-03 20:34:24 -0400

    adjusted env variables

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "lms_info" //name of the database
+    database: process.env.DB_NAME //name of the database
 }).promise() //use pool whenever accessing the database
 
 module.exports = pool;
